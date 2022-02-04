e4ae7f57272f018f026822ea5b4682cb206df610
luayida99 <e0543409@u.nus.edu>
2022-02-04 16:13:02 +0800

    do q1 code except marking

--- part1/autograder/grade.sh
+++ part1/autograder/grade.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 # Check if we have enough arguments
+if [ "$#" -ne 1 ]; then 
+    echo "Usage: ./grade.sh <filename>"
+fi
 
 # Delete temporary files
+rm -rfv ref/*.out
 
 # Compile the reference program
+gcc ref/*.c -o $1
 
 # Generate reference output files
+for i in ref/*.in; do 
+    echo >> $i.out 
+    ./$1 < $i > $i.out
+done
 
 # Now mark submissions
 
@@ -21,4 +30,3 @@
     # Compare with reference output files  and award 1 mark if they are identical
 # print score for student
 # print total files marked.
-    
